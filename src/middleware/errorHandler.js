2f4bfe7c40a26d5ec4c5aa3f92e576fa49010d06
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-11-16 15:07:20 +0200

    fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,20 +1,34 @@
+import { isCelebrateError } from "celebrate";
 import { HttpError } from "http-errors";
 
 export const errorHandler = (err, req, res, next) => {
   console.error("Error Middleware:", err);
 
-  // Якщо помилка створена через http-errors
+  // 1. Перевірка помилок celebrate
+  if (isCelebrateError(err)) {
+    const segment = ["body", "query", "params", "headers"]
+      .map(k => err.details.get(k))
+      .find(v => v);
+
+    return res.status(400).json({
+      message: segment.message,       // основне повідомлення про помилку
+      details: segment.details,       // масив Joi-помилок
+    });
+  }
+
+  // 2. http-errors
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
     });
   }
 
+  // 3. Інші помилки
   const isProd = process.env.NODE_ENV === "production";
 
   res.status(500).json({
     message: isProd
       ? "Something went wrong. Please try again later."
       : err.message,
- });
+  });
 };
