63cfb0907a71ab0366483f29075cd51bf34e5208
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-11-26 23:43:42 +0200

    reset password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail } from '../controllers/authController.js';
-import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from '../validations/authValidation.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser, requestResetEmail, resetPassword } from '../controllers/authController.js';
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -10,5 +10,6 @@ router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
 router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
